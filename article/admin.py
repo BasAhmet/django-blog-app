005164ae3f644a2f75d77b8c6a56091d28965b66
Ahmet Baş <94932629+BasAhmet@users.noreply.github.com>
2022-04-05 14:46:52 +0300

    Add files via upload

---
+++ article/admin.py
@@ -0,0 +1,21 @@
+from encodings import search_function
+from re import search
+from unittest.util import _MAX_LENGTH
+from xml.etree.ElementTree import Comment
+from click import command
+from django.contrib import admin
+from .models import Article, Comment
+
+# Register your models here.
+
+admin.site.register(Comment)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title","author","createDate"]
+    list_display_links = ["title","createDate"]
+    search_fields = ["title"]
+    list_filter = ["createDate"]
+    class Meta:
+        model = Article
+
